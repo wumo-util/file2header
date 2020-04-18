e75eb308cba76969af420a2b94cb5e24d18d29ce
wumo <wumo@outlook.com>
2020-04-18 15:32:51 +0800

    release plz.  seperate h/cpp

--- src/main.cpp
+++ src/main.cpp
@@ -4,13 +4,27 @@
 #include <regex>
 #include <cmath>
 
+inline char separator() {
+#ifdef _WIN32
+  return '\\';
+#else
+  return '/';
+#endif
+}
+
+bool endWith(std::string const &fullString, std::string const &ending) {
+  if(fullString.length() >= ending.length())
+    return 0 == fullString.compare(
+                  fullString.length() - ending.length(), ending.length(), ending);
+  else
+    return false;
+}
+
 int main(int argc, char **argv) {
   cxxopts::Options options("file2header", "helper to convert file to header");
   auto option = options.add_options();
   option("i,input", "input file", cxxopts::value<std::string>());
-  option(
-    "o,output", "output header file name without extension",
-    cxxopts::value<std::string>());
+  option("o,output", "output directory", cxxopts::value<std::string>());
   option("n,name", "variable name", cxxopts::value<std::string>());
   std::string input, output, name;
   try {
@@ -33,23 +47,35 @@ int main(int argc, char **argv) {
     buffer.resize(std::ceil(float(size) / sizeof(uint32_t)));
 
     f.read(reinterpret_cast<char *>(buffer.data()), size);
-    std::ofstream fout(output);
+
     std::regex escape("[^a-zA-Z0-9]");
     std::string cxxName;
     std::regex_replace(
       std::back_inserter(cxxName), name.begin(), name.end(), escape, "_");
+
+    output = (endWith(output, "/") || endWith(output, "\\")) ? output :
+                                                               output + separator();
+    std::ofstream fheader(output + cxxName + ".h");
+    std::ofstream fcpp(output + cxxName + ".cpp");
+
+    fheader
+      << "#pragma once" << std::endl
+      << "#include <cstdint>" << std::endl
+      << "const uint32_t " << cxxName + "_size=" << buffer.size() << ";" << std::endl
+      << "extern const uint32_t " << cxxName << "[" << cxxName + "_size];" << std::endl;
+
     auto i = 0, line = 10;
-    fout << "#pragma once" << std::endl
-         << "const uint32_t " << cxxName << "_size=" << buffer.size() << ";" << std::endl
+    fcpp << "#include \"" << cxxName << ".h"
+         << "\"" << std::endl
          << "const uint32_t " << cxxName << "[]={" << std::endl;
     for(auto &data: buffer) {
-      fout << "0x" << std::hex << data << ",";
+      fcpp << "0x" << std::hex << data << ",";
       if(i++ > line) {
-        fout << std::endl;
+        fcpp << std::endl;
         i = 0;
       }
     }
-    fout << "};" << std::endl;
+    fcpp << "};" << std::endl;
   } catch(std::exception &e) { std::cerr << e.what() << std::endl; }
   return 0;
 }
